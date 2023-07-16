6dec599d2f8106e9361491769468917737757d46
Asif <jrmdasif5@gmail.com>
2023-07-16 20:24:07 +0600

    Jwt token routes set up

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+var jwt = require('jsonwebtoken');
 
 
 // Middleware
@@ -13,6 +14,25 @@ app.use(express.json());
 app.get('/', (req, res)=> {
     res.send('Car doctor is running')
 });
+
+const verifyJWT = (req, res, next)=> {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({error: true, message: 'Unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+  console.log(token);
+  jwt.verify(token, process.env.JWT_SECRET_TOKEN, (err, decoded)=> {
+    if (err) {
+    return res.status(401).send({error: true, message: 'Unauthorized access'})    
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
+
+
 
 const uri =`mongodb+srv://${process.env.USER_Key}:${process.env.SECRET_Key}@cluster0.q4abhch.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,6 +45,8 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +55,18 @@ async function run() {
     const serviceCollection = client.db("car-doctor").collection('services');
     const checkoutCollection = client.db('car-doctor').collection('checkout');
 
+
+    // Jwt Routes
+    app.post ('/jwt', (req, res)=> {
+      const user = req.body;
+      console.log(user)
+      const token = jwt.sign(user, process.env.JWT_SECRET_TOKEN, {
+        expiresIn: '1h'
+      })
+      console.log(token)
+      res.send({token})
+    })
+    // Service Related Routes
 
     app.get('/services', async (req, res)=> {
         const cursor = serviceCollection.find();
@@ -47,7 +81,7 @@ async function run() {
       res.send(result)
     })
 
-    // Orders
+    // Orders Related Routes
     app.post('/checkout', async (req, res)=> {
       const check = req.body;
       const result = await checkoutCollection.insertOne(check);
@@ -55,7 +89,12 @@ async function run() {
       console.log(check)
     })
 
-    app.get('/checkout', async (req, res)=> {
+    app.get('/checkout',verifyJWT, async (req, res)=> {
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({error: 1, message: 'Forbidden Access'})
+      }
+      console.log('Decoded', decoded)
       let query = {};
       if(req.query?.email){
         query = {email: req.query.email}
